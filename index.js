e3316a4f5703cdd97123b9969e26ef4d3f3609a9
Jaden Down <24631328+jpdown@users.noreply.github.com>
2021-05-21 15:44:03 -0500

    convert to spaces (why was it tabs)

--- index.js
+++ index.js
@@ -13,75 +13,75 @@ const redirectUri = 'http://localhost';
 const client = new RPC.Client({transport: 'ipc'});
 
 function dispatch(xsmessage) {
-	let server = udp.createSocket("udp4");
-	server.send(xsmessage, 42069, "localhost", () => {
-		server.close();
-	});
+    let server = udp.createSocket("udp4");
+    server.send(xsmessage, 42069, "localhost", () => {
+        server.close();
+    });
 }
 
 function parseMessage(evt) {
-	let content_parsed = evt?.message?.content_parsed;
-	let content = "";
+    let content_parsed = evt?.message?.content_parsed;
+    let content = "";
 
-	if (content_parsed) {
-		for (let parse of content_parsed) {
-			switch(parse.type) {
-				case "text":
-					content += parse.content;
-					break;
-				case "mention":
-					content += parse.content[0].content;
-					break;
-				case "emoji":
-					content += parse.name;
-					break;
-			}
-		}
-	}
+    if (content_parsed) {
+        for (let parse of content_parsed) {
+            switch(parse.type) {
+                case "text":
+                    content += parse.content;
+                    break;
+                case "mention":
+                    content += parse.content[0].content;
+                    break;
+                case "emoji":
+                    content += parse.name;
+                    break;
+            }
+        }
+    }
 
-	return content;
+    return content;
 }
 
 function getIcon(evt, xsmessage) {
-	let icon = new Stream();
-	let base64 = "";
-	https.get(evt.icon_url, (response) => {
-		response.on('data', d => {
-			icon.push(d);
-		});
+    let icon = new Stream();
+    let base64 = "";
+    https.get(evt.icon_url, (response) => {
+        response.on('data', d => {
+            icon.push(d);
+        });
 
-		response.on('end', () => {
-			base64 = icon.read().toString("base64");
-			xsmessage.icon = base64;
+        response.on('end', () => {
+            base64 = icon.read().toString("base64");
+            xsmessage.icon = base64;
 
-			dispatch(JSON.stringify(xsmessage));
-		});
-	});
+            dispatch(JSON.stringify(xsmessage));
+        });
+    });
 
-	return base64;
+    return base64;
 }
 
 client.on('ready', () => {
-	console.log("Logged in as", client.application.name);
-	console.log("Authed for user", client.user.username);
+    console.log("Logged in as", client.application.name);
+    console.log("Authed for user", client.user.username);
 
-	client.subscribe("NOTIFICATION_CREATE", (evt) => {
-		let xsmessage = {
-			"messageType": 1,
-			"index": 0,
-			"timeout": 5,
-			"height": 175,
-			"opacity": 1,
-			"volume": 0.7,
-			"audioPath": "",
-			"title": `${evt.title} - Discord`,
-			"content": parseMessage(evt),
-			"useBase64Icon": true,
-			"sourceApp": "XSOverlay Discord RPC"
-		};
+    client.subscribe("NOTIFICATION_CREATE", (evt) => {
+        let xsmessage = {
+            "messageType": 1,
+            "index": 0,
+            "timeout": 5,
+            "height": 175,
+            "opacity": 1,
+            "volume": 0.7,
+            "audioPath": "",
+            "title": `${evt.title} - Discord`,
+            "content": parseMessage(evt),
+            "useBase64Icon": true,
+            "sourceApp": "XSOverlay Discord RPC"
+        };
 
-		getIcon(evt, xsmessage);
-	});
+        getIcon(evt, xsmessage);
+    });
 });
 
 client.login({ clientId: clientId, scopes: scopes, clientSecret: clientSecret, redirectUri: redirectUri });
